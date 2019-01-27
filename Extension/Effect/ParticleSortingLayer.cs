d8619f99d09d580a049643d47827c22d3159ab44
tiger <wangzhen00@gmail.com>
2019-01-27 18:37:23 +0900

    - added particle sorting layer class

---
+++ Extension/Effect/ParticleSortingLayer.cs
@@ -0,0 +1,30 @@
+/*
+ * @Author: zhen wang 
+ * @Date: 2019-01-27 11:14:04 
+ * @Last Modified by: zhen wang
+ * @Last Modified time: 2019-01-27 11:24:20
+ */
+
+
+using UnityEngine;
+
+
+namespace tiger.Extension.Effect
+{
+    [ExecuteInEditMode]
+    public class ParticleSortingLayer : MonoBehaviour
+    {
+        public string _layerName;
+
+        public int _layerSorting;
+
+
+
+        void OnEnable()
+        {
+            var particleSystem = transform.GetComponent<ParticleSystem>();
+            particleSystem.shape.spriteRenderer.sortingLayerName = _layerName;
+            particleSystem.shape.spriteRenderer.sortingOrder = _layerSorting;
+        }
+    }
+}
