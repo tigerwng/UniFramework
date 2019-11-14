6dabaa79fcdef36dd55259cea3818d57ac3484e0
tiger <wangzhen00@gmail.com>
2019-11-14 17:47:18 +0900

    - fixed some bugs

--- Extension/UI/ToggleButton/Editor/ToggleButtonEditor.cs
+++ Extension/UI/ToggleButton/Editor/ToggleButtonEditor.cs
@@ -11,75 +11,79 @@ using UnityEngine;
 using UnityEngine.UI;
 using zw.UniFramework.Extension.UI;
 
-[CustomEditor(typeof(ToggleButton), true)]
-[CanEditMultipleObjects]
-public class ToggleButtonEditor : Editor
+
+namespace zw.UniFramework.Extension.UI.Editor
 {
-        private SerializedObject m_serializedObject;
+    [CustomEditor(typeof(zw.UniFramework.Extension.UI.ToggleButton), true)]
+    [CanEditMultipleObjects]
+    public class ToggleButtonEditor : UnityEditor.Editor
+    {
+            private SerializedObject m_serializedObject;
 
-        private SerializedProperty m_InteractableProperty;
+            private SerializedProperty m_InteractableProperty;
 
-        private SerializedProperty m_ColorBlockProperty;
-        private SerializedProperty m_OnClickProperty;
+            private SerializedProperty m_ColorBlockProperty;
+            private SerializedProperty m_OnClickProperty;
 
-        private SerializedProperty m_onSourceProperty;
-        private SerializedProperty m_offSourceProperty;
+            private SerializedProperty m_onSourceProperty;
+            private SerializedProperty m_offSourceProperty;
 
-        private SerializedProperty m_TargetGraphicProperty;
+            private SerializedProperty m_TargetGraphicProperty;
 
-        private SerializedProperty m_IsOnProperty;
+            private SerializedProperty m_IsOnProperty;
 
-        private SerializedProperty m_isAutoUpdateProperty;
+            private SerializedProperty m_isAutoUpdateProperty;
 
-        void OnEnable()
-        {
-            m_serializedObject = new SerializedObject(target);
-            
-            m_InteractableProperty  = m_serializedObject.FindProperty("m_Interactable");
-            m_OnClickProperty       = m_serializedObject.FindProperty("m_OnClick");
-            m_ColorBlockProperty    = m_serializedObject.FindProperty("m_Colors");
-
-            m_onSourceProperty      = m_serializedObject.FindProperty("m_onSource");
-            m_offSourceProperty     = m_serializedObject.FindProperty("m_offSource");
-
-            m_TargetGraphicProperty = m_serializedObject.FindProperty("m_TargetGraphic");
-
-            m_IsOnProperty          = m_serializedObject.FindProperty("m_isOn");
-
-            m_isAutoUpdateProperty = m_serializedObject.FindProperty("m_isAutoUpdate");
-        }
-
-        public override void OnInspectorGUI()
-        {
-            m_serializedObject.Update();
-
-            EditorGUILayout.PropertyField(m_InteractableProperty);
-
-            EditorGUILayout.Space();
-
-            EditorGUILayout.PropertyField(m_IsOnProperty);
-
-            EditorGUILayout.PropertyField(m_isAutoUpdateProperty);
-
-            var graphic = m_TargetGraphicProperty.objectReferenceValue as Graphic;
-            if (graphic == null)
+            void OnEnable()
             {
-                graphic = (target as Selectable).GetComponent<Graphic>();
+                m_serializedObject = new SerializedObject(target);
+                
+                m_InteractableProperty  = m_serializedObject.FindProperty("m_Interactable");
+                m_OnClickProperty       = m_serializedObject.FindProperty("m_OnClick");
+                m_ColorBlockProperty    = m_serializedObject.FindProperty("m_Colors");
+
+                m_onSourceProperty      = m_serializedObject.FindProperty("m_onSource");
+                m_offSourceProperty     = m_serializedObject.FindProperty("m_offSource");
+
+                m_TargetGraphicProperty = m_serializedObject.FindProperty("m_TargetGraphic");
+
+                m_IsOnProperty          = m_serializedObject.FindProperty("m_isOn");
+
+                m_isAutoUpdateProperty = m_serializedObject.FindProperty("m_isAutoUpdate");
             }
 
-            EditorGUILayout.PropertyField(m_TargetGraphicProperty);
-            EditorGUILayout.PropertyField(m_onSourceProperty);
-            EditorGUILayout.PropertyField(m_offSourceProperty);
+            public override void OnInspectorGUI()
+            {
+                m_serializedObject.Update();
 
-            EditorGUILayout.Space();
+                EditorGUILayout.PropertyField(m_InteractableProperty);
 
-            EditorGUILayout.PropertyField(m_ColorBlockProperty);
+                EditorGUILayout.Space();
 
-            EditorGUILayout.Space();
+                EditorGUILayout.PropertyField(m_IsOnProperty);
 
-            EditorGUILayout.PropertyField(m_OnClickProperty);
-            
-            m_serializedObject.ApplyModifiedProperties();
-        }
+                EditorGUILayout.PropertyField(m_isAutoUpdateProperty);
 
+                var graphic = m_TargetGraphicProperty.objectReferenceValue as Graphic;
+                if (graphic == null)
+                {
+                    graphic = (target as Selectable).GetComponent<Graphic>();
+                }
+
+                EditorGUILayout.PropertyField(m_TargetGraphicProperty);
+                EditorGUILayout.PropertyField(m_onSourceProperty);
+                EditorGUILayout.PropertyField(m_offSourceProperty);
+
+                EditorGUILayout.Space();
+
+                EditorGUILayout.PropertyField(m_ColorBlockProperty);
+
+                EditorGUILayout.Space();
+
+                EditorGUILayout.PropertyField(m_OnClickProperty);
+                
+                m_serializedObject.ApplyModifiedProperties();
+            }
+
+    }
 }
